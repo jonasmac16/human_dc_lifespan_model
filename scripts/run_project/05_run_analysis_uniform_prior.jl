645abc35c8e2eb9c2cbe2bf248219bcb0d2ebbee
Jonas Mackerodt <jonas.mackerodt@kennedy.ox.ac.uk>
2024-02-08 16:37:50 +0000

    added analysis run scripts for uniform prior notebooks

---
+++ scripts/run_project/05_run_analysis_uniform_prior.jl
@@ -0,0 +1,21 @@
+projectdir_name = dirname(dirname(@__DIR__))
+
+using Pkg
+Pkg.activate(projectdir_name)
+
+using DrWatson
+DrWatson.@quickactivate "Model of DC Differentiation"
+
+analysis_notebooks = readdir(projectdir("notebooks", "03_analysis"))
+
+include(projectdir("scripts", "run_project", "00_threads.jl"))
+
+for j in analysis_notebooks[[1,2,3,4]]
+    println("Started running " * j)
+    println("...")
+    file_tmp = projectdir("notebooks", "03_analysis", j, j*".jl")
+    cmd_tmp = `julia -t $n_threads $file_tmp`
+    run(cmd_tmp)
+    println("Finished running " * j)
+    println("")
+end
