0bbf2bbc857318c8ae7c1d048c0a1ae40778d9d3
Jonas Mackerodt <jonas.mackerodt@kennedy.ox.ac.uk>
2024-02-08 14:05:42 +0000

    make setup script more robust

--- scripts/run_project/01_run_setup.jl
+++ scripts/run_project/01_run_setup.jl
@@ -4,9 +4,27 @@ using Pkg
 Pkg.activate(projectdir_name)
 Pkg.instantiate()
 
-
-ENV["R_HOME"] = "*"
-Pkg.build("RCall")
-
 using Conda
-Conda.add("r-loo", channel = "conda-forge")
+Conda.add("mamba")
+ENV["CONDA_JL_CONDA_EXE"] = joinpath(Conda.ROOTENV, "bin", "mamba")
+Pkg.build("Conda")
+
+# build PyCall
+ENV["PYTHON"] = ""
+Pkg.build("PyCall")
+
+
+# Install important libraries and (make sure conda is v 23.1.0)
+Conda.add("r-base")
+Conda.add("r-loo")
+Conda.add("matplotlib")
+
+
+# build RCall
+ENV["R_HOME"] = ENV["HOME"]*"/.julia/conda/3/lib/R/"
+# Potentially you need to link to theup-to-date system libstdc++.so.6 due to outdated version packaged with julia 
+# see https://discourse.julialang.org/t/glibcxx-3-4-26-not-found-in-rcall/29113/10?u=laborg
+# and https://github.com/JuliaLang/julia/issues/34276
+# e.g. for Ubuntu 19.10 64bit - match your locations accordingly!
+# cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 $JULIA_HOME/lib/julia/
+Pkg.build("RCall")
