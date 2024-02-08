ff64a891732b718d36b3c4d4ccee5086cf91575f
Jonas Mackerodt <jonas.mackerodt@kennedy.ox.ac.uk>
2024-02-08 14:05:27 +0000

    increase MCMC samples

--- scripts/run_project/00_mcmc_settings.jl
+++ scripts/run_project/00_mcmc_settings.jl
@@ -1,4 +1,4 @@
-mcmc_iters = 300
+mcmc_iters = 4000
 n_chains = 4
 parallel_sampling_method = MCMCThreads() #nothing or MCMCThreads()
 solver_parallel_methods = EnsembleThreads() #EnsembleSerial() or #EnsembleThreads() for parallel solving of ODEs
